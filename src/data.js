6d100d120f67ac7f4d4d21cabede6e477746cd70
EPeng <erpengic@gmail.com>
2021-06-23 14:02:14 -0500

    first init

---
+++ src/data.js
@@ -0,0 +1,60 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+export const skills = [
+    'Javascript',
+    'NodeJS',
+    'Express',
+    'React',
+    'HTML',
+    'CSS'
+]
+
+export const testimonials = [
+   {
+       quote: 'liqua adipisicing in anim et nostrud reprehenderit id irure qui eu duis eiusmod. Laboris reprehenderit dolore minim nostrud enim labore magna exercitation enim reprehenderit labore. Fugiat irure culpa aute anim ea sit duis labore nulla ad amet dolore cupidatat enim. Tempor Lorem quis cillum velit ut reprehenderit culpa ullamco incididunt ullamco voluptate occaecat ipsum commodo. Est eiusmod pariatur ipsum veniam.',
+      image: null,
+      name: 'Bob Bobberson',
+      company: 'Bobs Burgers' 
+   },
+   {
+    quote: 'liqua adipisicing in anim et nostrud reprehenderit id irure qui eu duis eiusmod. Laboris reprehenderit dolore minim nostrud enim labore magna exercitation enim reprehenderit labore. Fugiat irure culpa aute anim ea sit duis labore nulla ad amet dolore cupidatat enim. Tempor Lorem quis cillum velit ut reprehenderit culpa ullamco incididunt ullamco voluptate occaecat ipsum commodo. Est eiusmod pariatur ipsum veniam.',
+   image: null,
+   name: 'Joe Hames',
+   company: 'Joes Sandwiches' 
+    }
+]
